0ba2fbea50df1181fdb803a2bcdf23fc955b0dd7
Thomas Smith <thomathom@gmail.com>
2012-01-18 16:04:02 -0500

    Use Strategy pattern instead of right/left views

--- edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/BipartiteGraphRenderer.java
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/BipartiteGraphRenderer.java
@@ -5,7 +5,6 @@ import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -40,8 +39,8 @@ public class BipartiteGraphRenderer implements Paintable {
 	
 	private LinkedHashMap<Node, NodeView> placeNodes() {
 		LinkedHashMap<Node,NodeView> nodeViews = Maps.newLinkedHashMap();
-		nodeViews.putAll(placeNodesOnLine(data.getRightNodes(), getRightLine()));
-		nodeViews.putAll(placeNodesOnLine(data.getLeftNodes(), getLeftLine()));
+		nodeViews.putAll(placeNodesOnLine(data.getRightNodes(), getRightLine(), NodeView.LabelPainter.RIGHT));
+		nodeViews.putAll(placeNodesOnLine(data.getLeftNodes(), getLeftLine(), NodeView.LabelPainter.LEFT));
 		
 		for (NodeView nv : nodeViews.values()) {
 			painter.add(nv);
@@ -51,14 +50,14 @@ public class BipartiteGraphRenderer implements Paintable {
 	}
 
 	private LinkedHashMap<Node, NodeView> placeNodesOnLine(ImmutableList<Node> nodes,
-			Line2D centerLine) {
+			Line2D centerLine, NodeView.LabelPainter painter) {
 		LinkedHashMap<Node,NodeView> nodeViews = Maps.newLinkedHashMap();
 		int numNodes = nodes.size();
 		double denominator = Math.max(1, numNodes - 1); // don't divide by 0!
 		
 		for (int i = 0; i < numNodes; i++) {
 			Point2D centerPoint = LayoutUtils.getPointAlongLine(centerLine, i / denominator);
-			NodeView view = NodeView.create(nodes.get(i), centerPoint);
+			NodeView view = new NodeView(nodes.get(i), centerPoint, painter);
 			nodeViews.put(nodes.get(i), view);
 		}
 		return nodeViews;
--- edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/component/NodeView.java
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/component/NodeView.java
@@ -1,26 +1,63 @@
 package edu.iu.sci2.visualization.bipartitenet.component;
 
 import java.awt.Graphics2D;
+import java.awt.font.TextLayout;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
+import edu.iu.sci2.visualization.bipartitenet.LayoutUtils;
+import edu.iu.sci2.visualization.bipartitenet.component.NodeView.LabelPainter;
 import edu.iu.sci2.visualization.bipartitenet.model.Node;
 
-public abstract class NodeView implements Paintable {
-	private Node node;
-	private Point2D nodeCenter;
+public class NodeView implements Paintable {
+	public enum LabelPainter {
+		LEFT {
+
+			@Override
+			void paintLabel(NodeView nv, Graphics2D g) {
+				// TODO Auto-generated method stub
+				TextLayout tl = new TextLayout(nv.getNode().getLabel(),
+						g.getFont(), g.getFontRenderContext());
+				Rectangle2D textBounds = tl.getBounds();
+				int x = (int) (nv.getNodeCenter().getX()
+						- nv.getCenterToTextDistance() - textBounds.getWidth());
+
+				g.drawString(
+						nv.getNode().getLabel(),
+						x,
+						(int) nv.getNodeCenter().getY()
+								+ LayoutUtils.getFontCenterHeight(g));
+			}
+
+		},
+		RIGHT {
+			@Override
+			void paintLabel(NodeView nv, Graphics2D g) {
+				g.drawString(
+						nv.getNode().getLabel(),
+						(int) nv.getNodeCenter().getX()
+								+ nv.getCenterToTextDistance(),
+						(int) nv.getNodeCenter().getY()
+								+ LayoutUtils.getFontCenterHeight(g));
+			}
+		};
+
+		abstract void paintLabel(NodeView nv, Graphics2D g);
+
+	}
+
+	private final Node node;
+	private final Point2D nodeCenter;
+	private LabelPainter labelPainter;
 	private static final int NODE_TEXT_PADDING = 8;
 	private static final int NODE_MAX_RADIUS = 15;
 
-	protected NodeView(Node node, Point2D nodeCenter) {
+	public NodeView(Node node, Point2D nodeCenter, LabelPainter painter) {
 		super();
 		this.node = node;
 		this.nodeCenter = nodeCenter;
+		this.labelPainter = painter;
 	}
-	
-	public static NodeView create(Node node, Point2D nodeCenter) {
-		return new RightNodeView(node, nodeCenter);
-	}
-	
 
 	public int getCenterToTextDistance() {
 		return getMaxRadius() + NODE_TEXT_PADDING;
@@ -42,8 +79,7 @@ public abstract class NodeView implements Paintable {
 	public void paint(Graphics2D g) {
 		g.drawOval((int) nodeCenter.getX(), (int) nodeCenter.getY(),
 				(int) node.getValue(), (int) node.getValue());
-		paintLabel(g);
+		labelPainter.paintLabel(this, g);
 	}
 
-	public abstract void paintLabel(Graphics2D g);
 }
