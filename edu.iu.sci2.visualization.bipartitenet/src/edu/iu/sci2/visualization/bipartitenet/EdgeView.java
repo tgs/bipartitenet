f42ddb91430fab889160d78a6a4095b63cb9b6c5
Thomas Smith <thomathom@gmail.com>
2012-01-19 14:16:10 -0500

    arrow heads!

--- edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/EdgeView.java
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/EdgeView.java
@@ -4,13 +4,19 @@ import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.Line2D;
 
+import math.geom2d.AffineTransform2D;
+import math.geom2d.Point2D;
+import math.geom2d.line.AbstractLine2D;
 import math.geom2d.line.LineSegment2D;
+import math.geom2d.polygon.Polygon2DUtils;
+import math.geom2d.polygon.SimplePolygon2D;
 import edu.iu.sci2.visualization.bipartitenet.component.NodeView;
 import edu.iu.sci2.visualization.bipartitenet.component.Paintable;
 
 public class EdgeView implements Paintable {
 	
 	private static final double NODE_EDGE_SPACE = 4;
+	private static final double ARROW_SIDE_LENGTH = 6;
 	private final NodeView dest;
 	private final NodeView src;
 
@@ -24,8 +30,22 @@ public class EdgeView implements Paintable {
 		LineSegment2D grossLine = new LineSegment2D(src.getNodeCenter(), dest.getNodeCenter());
 		double tStart = (getRadius(src) + NODE_EDGE_SPACE) / grossLine.getLength(),
 				tEnd = (getRadius(dest) + NODE_EDGE_SPACE) / grossLine.getLength();
-		grossLine.getSubCurve(tStart, 1 - tEnd).draw(g);
+		AbstractLine2D fineLine = grossLine.getSubCurve(tStart, 1 - tEnd);
+		
+		fineLine.draw(g);
 
+		drawArrowHead(fineLine, g);
+		
+	}
+
+	private void drawArrowHead(AbstractLine2D line, Graphics2D g) {
+		Point2D end = line.getLastPoint();
+		Point2D colinearPoint = line.getPoint(1 - (ARROW_SIDE_LENGTH / line.getLength()));
+		Point2D cwPoint = colinearPoint.transform(AffineTransform2D.createRotation(end, Math.PI / 6));
+		Point2D ccwPoint = colinearPoint.transform(AffineTransform2D.createRotation(end, -Math.PI / 6));
+		SimplePolygon2D arrowHead = new SimplePolygon2D(end, cwPoint, ccwPoint);
+		arrowHead.draw(g);
+		arrowHead.fill(g);
 	}
 
 	private double getRadius(NodeView n) {
