478270f48836a933c0cb8b55e81731719641662b
Thomas Smith <thomathom@gmail.com>
2012-01-19 15:24:01 -0500

    Build in skeleton of OSGi service framework

---
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/algorithm/BipartiteNetAlgorithm.java
@@ -0,0 +1,31 @@
+package edu.iu.sci2.visualization.bipartitenet.algorithm;
+
+import java.util.Dictionary;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmExecutionException;
+import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.data.Data;
+
+public class BipartiteNetAlgorithm implements Algorithm {
+
+	@Override
+	public Data[] execute() throws AlgorithmExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public class Factory implements AlgorithmFactory {
+
+		@Override
+		public Algorithm createAlgorithm(Data[] data,
+				Dictionary<String, Object> parameters,
+				CIShellContext ciShellContext) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+}
