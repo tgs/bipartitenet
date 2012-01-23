e9505581cb915eaf6efd5ed9334903cc7e95e71e
Thomas Smith <thomathom@gmail.com>
2012-01-23 17:32:34 -0500

    Hey it works with Sci2 now!

--- edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/algorithm/BipartiteNetAlgorithm.java
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/algorithm/BipartiteNetAlgorithm.java
@@ -1,16 +1,51 @@
 package edu.iu.sci2.visualization.bipartitenet.algorithm;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+import math.geom2d.line.LineSegment2D;
+
 import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.algorithm.AlgorithmExecutionException;
 import org.cishell.framework.data.Data;
 
+import edu.iu.nwb.util.nwbfile.ParsingException;
+import edu.iu.sci2.visualization.bipartitenet.BipartiteGraphRenderer;
+import edu.iu.sci2.visualization.bipartitenet.component.CanvasContainer;
+import edu.iu.sci2.visualization.bipartitenet.model.BipartiteGraphDataModel;
+import edu.iu.sci2.visualization.bipartitenet.model.NWBDataImporter;
+
 public class BipartiteNetAlgorithm implements Algorithm {
+
+	public BipartiteNetAlgorithm(File nwbFile, String nodeSizeColumn,
+			String leftSideType) throws FileNotFoundException, IOException,
+			ParsingException {
+		NWBDataImporter importer = new NWBDataImporter("bipartitetype",
+				leftSideType, nodeSizeColumn);
+		BipartiteGraphDataModel model = importer
+				.constructModelFromFile(new FileInputStream(nwbFile));
+		
+		// XXX this should be in .execute() below!
+		
+		JFrame f = new JFrame("Bipartite Network Graph");
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		f.setSize(600, 600);
+		CanvasContainer cc = new CanvasContainer();
+		BipartiteGraphRenderer r = new BipartiteGraphRenderer(
+				model, new LineSegment2D(200, 100, 200, 500),
+				new LineSegment2D(400, 100, 400, 500));
+		cc.add(r);
+		f.getContentPane().add(cc);
+		f.setVisible(true);
+	}
 
 	@Override
 	public Data[] execute() throws AlgorithmExecutionException {
 		return new Data[] {};
 	}
-	
-	
 
 }
--- edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/algorithm/BipartiteNetAlgorithmFactory.java
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/algorithm/BipartiteNetAlgorithmFactory.java
@@ -1,18 +1,77 @@
 package edu.iu.sci2.visualization.bipartitenet.algorithm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Dictionary;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmCreationFailedException;
 import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.algorithm.ParameterMutator;
 import org.cishell.framework.data.Data;
+import org.cishell.utilities.mutateParameter.dropdown.DropdownMutator;
+import org.osgi.service.metatype.ObjectClassDefinition;
 
-public class BipartiteNetAlgorithmFactory implements AlgorithmFactory {
+import com.google.common.collect.Lists;
+
+import edu.iu.nwb.util.nwbfile.NWBFileParser;
+import edu.iu.nwb.util.nwbfile.NWBFileUtilities;
+import edu.iu.nwb.util.nwbfile.ParsingException;
+
+public class BipartiteNetAlgorithmFactory implements AlgorithmFactory,
+		ParameterMutator {
+	private static final String LEFT_SIDE_TYPE_ID = "leftSideType";
+	private static final String NODE_SIZE_COLUMN_ID = "nodeSizeColumn";
 
 	@Override
 	public Algorithm createAlgorithm(Data[] data,
 			Dictionary<String, Object> parameters, CIShellContext ciShellContext) {
-		return new BipartiteNetAlgorithm();
+		try {
+			return new BipartiteNetAlgorithm(getNWBFile(data),
+					(String) parameters.get(NODE_SIZE_COLUMN_ID),
+					(String) parameters.get(LEFT_SIDE_TYPE_ID));
+		} catch (FileNotFoundException e) {
+			throw new AlgorithmCreationFailedException(e);
+		} catch (IOException e) {
+			throw new AlgorithmCreationFailedException(e);
+		} catch (ParsingException e) {
+			throw new AlgorithmCreationFailedException(e);
+		}
+	}
+
+	@Override
+	public ObjectClassDefinition mutateParameters(Data[] data,
+			ObjectClassDefinition oldParameters) {
+		NWBFileExaminer examiner = new NWBFileExaminer();
+		File nwbFile = getNWBFile(data);
+
+		try {
+			new NWBFileParser(nwbFile).parse(examiner);
+		} catch (ParsingException e) {
+			throw new AlgorithmCreationFailedException(e);
+		} catch (IOException e) {
+			throw new AlgorithmCreationFailedException(e);
+		}
+
+		LinkedHashMap<String, String> nodeSchema = examiner.getNodeSchema();
+		List<String> numericColumns = 
+				NWBFileUtilities.findNumericAttributes(
+						NWBFileUtilities.removeRequiredNodeProps(nodeSchema));
+
+		DropdownMutator mutator = new DropdownMutator();
+
+		mutator.add(LEFT_SIDE_TYPE_ID, examiner.getBipartiteTypes());
+		mutator.add(NODE_SIZE_COLUMN_ID, numericColumns);
+
+		return mutator.mutate(oldParameters);
+	}
+
+	private File getNWBFile(Data[] data) {
+		return (File) data[0].getData();
 	}
 
 }
