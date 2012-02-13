5ba202ab0df33c2e06d19ed9f3bd62007f9f3b1a
Thomas Smith <thomathom@gmail.com>
2012-02-13 15:56:55 -0500

    NodeView: eliminate redundant NodeDestination

--- edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/BipartiteGraphRenderer.java
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/BipartiteGraphRenderer.java
@@ -18,7 +18,6 @@ import edu.iu.sci2.visualization.bipartitenet.component.PaintableContainer;
 import edu.iu.sci2.visualization.bipartitenet.model.BipartiteGraphDataModel;
 import edu.iu.sci2.visualization.bipartitenet.model.Edge;
 import edu.iu.sci2.visualization.bipartitenet.model.Node;
-import edu.iu.sci2.visualization.bipartitenet.model.NodeDestination;
 import edu.iu.sci2.visualization.bipartitenet.scale.Scale;
 
 public class BipartiteGraphRenderer implements Paintable {
@@ -60,8 +59,8 @@ public class BipartiteGraphRenderer implements Paintable {
 
 	private LinkedHashMap<Node, NodeView> placeNodes() {
 		LinkedHashMap<Node,NodeView> nodeViews = Maps.newLinkedHashMap();
-		nodeViews.putAll(placeNodesOnLine(data.getRightNodes(), getRightLine(), NodeDestination.RIGHT));
-		nodeViews.putAll(placeNodesOnLine(data.getLeftNodes(), getLeftLine(), NodeDestination.LEFT));
+		nodeViews.putAll(placeNodesOnLine(data.getRightNodes(), getRightLine()));
+		nodeViews.putAll(placeNodesOnLine(data.getLeftNodes(), getLeftLine()));
 		
 		for (NodeView nv : nodeViews.values()) {
 			painter.add(nv);
@@ -71,7 +70,7 @@ public class BipartiteGraphRenderer implements Paintable {
 	}
 
 	private LinkedHashMap<Node, NodeView> placeNodesOnLine(ImmutableList<Node> nodes,
-			LineSegment2D centerLine, NodeDestination painter) {
+			LineSegment2D centerLine) {
 		LinkedHashMap<Node,NodeView> nodeViews = Maps.newLinkedHashMap();
 		int numNodes = nodes.size();
 		double denominator = Math.max(1, numNodes - 1); // don't divide by 0!
@@ -79,7 +78,7 @@ public class BipartiteGraphRenderer implements Paintable {
 		
 		for (int i = 0; i < numNodes; i++) {
 			Point2D centerPoint = centerLine.getPoint(i / denominator);
-			NodeView view = new NodeView(nodes.get(i), centerPoint, painter, nodeRadiusCoding, maxHeight);
+			NodeView view = new NodeView(nodes.get(i), centerPoint, nodeRadiusCoding, maxHeight);
 			nodeViews.put(nodes.get(i), view);
 		}
 		return nodeViews;
--- edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/component/NodeView.java
+++ edu.iu.sci2.visualization.bipartitenet/src/edu/iu/sci2/visualization/bipartitenet/component/NodeView.java
@@ -7,24 +7,20 @@ import math.geom2d.Point2D;
 import math.geom2d.conic.Circle2D;
 import edu.iu.sci2.visualization.bipartitenet.PageDirector;
 import edu.iu.sci2.visualization.bipartitenet.model.Node;
-import edu.iu.sci2.visualization.bipartitenet.model.NodeDestination;
 import edu.iu.sci2.visualization.bipartitenet.scale.Scale;
 
 public class NodeView implements Paintable {
+	public static final int NODE_TEXT_PADDING = 8;
+	
 	private final Node node;
 	private final Point2D nodeCenter;
-	
-	// TODO remove this, rely on Node's
-	private final NodeDestination leftRightDifference;
 	private final Scale<Double,Double> coding;
-	public static final int NODE_TEXT_PADDING = 8;
 	private final double maxHeight;
 
-	public NodeView(Node node, Point2D nodeCenter, NodeDestination painter, Scale<Double,Double> coding, double maxHeight) {
+	public NodeView(Node node, Point2D nodeCenter, Scale<Double,Double> coding, double maxHeight) {
 		super();
 		this.node = node;
 		this.nodeCenter = nodeCenter;
-		this.leftRightDifference = painter;
 		this.coding = coding;
 		this.maxHeight = maxHeight;
 	}
@@ -52,8 +48,8 @@ public class NodeView implements Paintable {
 	@Override
 	public void paint(Graphics2D g) {
 		Circle2D circle = new Circle2D(nodeCenter.getX(), nodeCenter.getY(), getRadius());
-		leftRightDifference.paintLabel(this, g, maxHeight);
-		g.setColor(leftRightDifference.getFillColor());
+		node.getDestination().paintLabel(this, g, maxHeight);
+		g.setColor(node.getDestination().getFillColor());
 		circle.fill(g);
 		g.setColor(Color.black);
 		circle.draw(g);
